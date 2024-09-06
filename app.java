12137f5fea33c70f5c75bcb20e74422074a61ae1
Daniel Paim Frantz <daniel.paim@edu.pucrs.br>
2024-09-06 18:37:01 -0300

    Arrumei o negócio de diminuir chance, tava só diminuindo quando acerta

--- app.java
+++ app.java
@@ -5,14 +5,14 @@ public class app{
         System.out.print("Teste usando Git");
         
         
-        System.out.print("Insira o primeiro valor");
+        System.out.println("Insira o primeiro valor");
         int primeiroValor=teclado.nextInt();
         
         System.out.println("Insira o segundo valor:");
         int segundoValor=teclado.nextInt();
 
         int resultado=primeiroValor+segundoValor;
-
+        teclado.close();
 
         System.out.println("Resultado "+ resultado);
     }
