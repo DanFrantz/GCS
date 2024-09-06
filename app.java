629e9d65f835242cc1bcc196555ca3175a9ac991
Daniel Paim Frantz <daniel.paim@edu.pucrs.br>
2024-09-06 18:13:28 -0300

    Adicionando entrada do usuário

--- app.java
+++ app.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class app{
     public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in)
         System.out.print("Teste usando Git");
-
-        int primeiroValor=10;
-        int segundoValor=20;
+        
+        
+        System.out.print("Insira o primeiro valor");
+        int primeiroValor=teclado.nextInt();
+        
+        System.out.println("Insira o segundo valor:");
+        int segundoValor=teclado.nextInt();
 
         int resultado=primeiroValor+segundoValor;
 
