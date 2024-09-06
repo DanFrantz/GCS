9acb5fbc9a24d25c8bd482aed352c295be54cdad
Daniel Paim Frantz <daniel.paim@edu.pucrs.br>
2024-09-06 18:09:37 -0300

    Somador e Arquivo txt info

--- app.java
+++ app.java
@@ -1,5 +1,13 @@
 public class app{
     public static void main(String[] args) {
         System.out.print("Teste usando Git");
+
+        int primeiroValor=10;
+        int segundoValor=20;
+
+        int resultado=primeiroValor+segundoValor;
+
+
+        System.out.println("Resultado"+ resultado);
     }
 }
